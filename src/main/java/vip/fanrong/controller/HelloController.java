5575ad5ead966a483d93ca2639a7a3954f13b8bd
范荣 <fanrong330@gmail.com>
2017-07-14 15:13:00 +0800

    Json Object Node used as api response

--- src/main/java/vip/fanrong/controller/HelloController.java
+++ src/main/java/vip/fanrong/controller/HelloController.java
@@ -1,5 +1,6 @@
 package vip.fanrong.controller;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -7,19 +8,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import vip.fanrong.common.util.DateUtil;
+import vip.fanrong.common.util.JsonUtil;
 
 /**
  * Created by Rong on 2017/7/13.
  */
 @RestController
 @RequestMapping(value = "/v1/hello") // parent folder for request mapping path
-@Api(value = "HelloWorld API", description = "第一版")
+@Api(value = "HelloWorld API", description = "v1")
 public class HelloController {
 
-    @ApiOperation(value = "获取消息", notes = "获取第一个HelloWorld消息")
+    @ApiOperation(value = "获取问候消息", notes = "获取来自系统的问候消息")
     @ApiImplicitParam(paramType = "path", name = "name", value = "姓名", required = true, dataType = "String")
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String sayHello(@PathVariable String name) {
-        return "Hello World！" + name;
+    public ObjectNode sayHello(@PathVariable String name) {
+        ObjectNode objectNode = JsonUtil.createObjectNode();
+        objectNode.put("name", name);
+        objectNode.put("greeting", "你好！");
+        objectNode.put("date", DateUtil.getDateNow());
+        return objectNode;
     }
 }
