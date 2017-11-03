72ade2f3554dfb6a0727ad37fe3366c8fb68f65f
fanrong <fanrong@chinatelecom.cn>
2017-11-03 09:19:32 +0800

    remove unused import statements

--- src/main/java/vip/fanrong/ScheduleConfiguration.java
+++ src/main/java/vip/fanrong/ScheduleConfiguration.java
@@ -2,13 +2,10 @@ package vip.fanrong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import vip.fanrong.service.KdsCrawlerService;
-
-import java.util.Calendar;
 
 /**
  * Created by Rong on 2017/10/30.
